86026e6286c325efc3cd346581eb3d2d568fe2ae
R. Tyler Croy <tyler@monkeypox.org>
2014-03-23 15:21:34 -0700

    Move the Aruba helper code into it's own file and fix our invocations of it

---
+++ features/support/aruba.rb
@@ -0,0 +1,62 @@
+require 'aruba'
+require 'aruba/cucumber'
+require 'aruba/in_process'
+
+
+# Forcing the Readline editor to be unavailable regardless of whether the
+# 'readline' module exists or not. This will make sure our mucking with $stdin
+# works out properly in the Aruba::InProcess runner
+class Thor
+  module LineEditor
+    class Readline
+      def self.available?
+        false
+      end
+    end
+  end
+end
+
+module JPM
+  module Cucumber
+    class CLIRunner
+      attr_reader :stdin
+
+      def initialize(argv, stdin=STDIN, stdout=STDOUT, stderr=STDERR, kernel=Kernel)
+        @argv, @stdin, @stdout, @stderr, @kernel = argv, stdin, stdout, stderr, kernel
+      end
+
+      def execute!
+        # Borrowed from
+        # <https://github.com/erikhuda/thor/wiki/Integrating-with-Aruba-In-Process-Runs>
+        exit_code = begin
+                      # Thor accesses these streams directly rather than
+                      # letting them be injected, so we replace them...
+                      $stderr = @stderr
+                      $stdin = @stdin
+                      $stdout = @stdout
+
+                      # Run our normal Thor app the way we know and love.
+                      JPM::CLI.start(@argv)
+
+                      # Thor::Base#start does not have a return value, assume
+                      # success if no exception is raised.
+                      0
+                    rescue Exception => ex
+                      # Proxy any exception that comes out of Thor itself back
+                      # to stderr
+                      $stderr.write(e.message + "\n")
+                      1
+                    ensure
+                      $stderr = STDERR
+                      $stdin = STDERR
+                      $stdout = STDERR
+                    end
+
+        @kernel.exit(exit_code)
+      end
+    end
+  end
+end
+
+Aruba::InProcess.main_class = JPM::Cucumber::CLIRunner
+Aruba.process = Aruba::InProcess
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,34 +1,10 @@
 require 'rubygems'
-require 'aruba'
-require 'aruba/cucumber'
-require 'aruba/in_process'
 require 'cucumber/rspec/doubles'
 
 require 'jpm/cli'
 
-module JPM
-  module Cucumber
-    class ArubaShell < Thor::Shell::Basic
-      attr_reader :stdin, :stdout, :stderr
-      def initialize(stdin, stdout, stderr)
-        super()
-        @stdin = stdin
-        @stdout = stdout
-        @stderr
-      end
-    end
-
-    class CLIRunner
-      def initialize(argv, stdin=STDIN, stdout=STDOUT, stderr=STDERR, kernel=Kernel)
-        @argv, @stdin, @stdout, @stderr, @kernel = argv, stdin, stdout, stderr, kernel
-      end
-
-      def execute!
-        JPM::CLI.start(@argv, :shell => ArubaShell.new(@stdin, @stdout, @stderr))
-      end
-    end
-  end
+unless RUBY_PLATFORM == 'java'
+  require 'debugger'
+  require 'debugger/pry'
 end
 
-Aruba::InProcess.main_class = JPM::Cucumber::CLIRunner
-Aruba.process = Aruba::InProcess
